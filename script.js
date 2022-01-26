bf50d3e2b3c07ff19d42981a0757cadc598e67a9
AishaniBissesar <94417167+AishaniBissesar@users.noreply.github.com>
2022-01-26 16:32:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.querySelector("button");
+var element = document.querySelector("div");
+
+button.addEventListener("click", function()  {
+    element.classList.toggle("....");
+});
